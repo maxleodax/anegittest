2b7774facbfc82180d999bc7711b89d0d0525da9
maxleodax <max.leodax@hotmail.com>
2014-05-27 15:46:52 +0200

    A great commit

---
+++ TestGit/src/ch/grabicus/git/second/AnotherClass.java
@@ -0,0 +1,7 @@
+package ch.grabicus.git.second;
+
+public class AnotherClass {
+  public AnotherClass() {
+    // TODO Auto-generated constructor stub
+  }
+}
--- TestGit/src/ch/grabicus/git/second/MyKlass.java
+++ TestGit/src/ch/grabicus/git/second/MyKlass.java
@@ -3,5 +3,6 @@ package ch.grabicus.git.second;
 public class MyKlass {
   public static void main(String[] args) {
     System.out.println("Hello GIT, you're cool!");
+    System.out.println("Bye");
   }
 }
