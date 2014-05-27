870cb6a238daf4cf54a9453bcab90b59f949a136
maxleodax <max.leodax@hotmail.com>
2014-05-27 15:58:26 +0200

    commitee commit

--- TestGit/src/ch/grabicus/git/second/MyKlass.java
+++ TestGit/src/ch/grabicus/git/second/MyKlass.java
@@ -2,6 +2,6 @@ package ch.grabicus.git.second;
 
 public class MyKlass {
   public static void main(String[] args) {
-    System.out.println("Hello GIT, you're cool!");
+    System.out.println("Hello GITy GATy, you're cool!");
   }
 }
