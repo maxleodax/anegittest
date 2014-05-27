4facd6cdf5e4e6ed21bdd7e1152324e58ecdec43
maxleodax <max.leodax@hotmail.com>
2014-05-27 15:36:01 +0200

    second commit

--- TestGit/src/ch/grabicus/git/second/MyKlass.java
+++ TestGit/src/ch/grabicus/git/second/MyKlass.java
@@ -2,6 +2,6 @@ package ch.grabicus.git.second;
 
 public class MyKlass {
   public static void main(String[] args) {
-    System.out.println("Hello GIT");
+    System.out.println("Hello GIT, you're cool!");
   }
 }
