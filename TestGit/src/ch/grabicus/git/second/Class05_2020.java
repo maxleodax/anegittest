02ab5bbc62d7817bb74f43b2ae3080c6cd5da2f6
maxleodax <max.leohub@hotmail.com>
2020-02-23 00:31:52 +0100

    branch 01 commit 03

--- TestGit/src/ch/grabicus/git/second/Class05_2020.java
+++ TestGit/src/ch/grabicus/git/second/Class05_2020.java
@@ -2,6 +2,7 @@ package ch.grabicus.git.second;
 
 public class Class05_2020 {
 	public static void main(String[] args) {
-		System.out.println("Class05_2020.main()");
+		System.out.println("ENTER Class05_2020.main()");
+		System.out.println("EXIT Class05_2020.main()");
 	}
 }
