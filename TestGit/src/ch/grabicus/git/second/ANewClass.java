7808333ce9d7f0bde22856d17ef743349de8b044
maxleodax <max.leohub@hotmail.com>
2020-02-23 02:59:41 +0100

    class5.6

--- TestGit/src/ch/grabicus/git/second/ANewClass.java
+++ TestGit/src/ch/grabicus/git/second/ANewClass.java
@@ -8,6 +8,7 @@ public class ANewClass {
 		System.out.println("ANewClass 3");
 		System.out.println("ANewClass 4");
 		System.out.println("ANewClass 5");
+		System.out.println("ANewClass 6");
 		System.out.println("EXIT main");
 	}
 }
