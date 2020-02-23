0e844e5542a02cf6c4bdb177b111971ab6d474e0
maxleodax <max.leohub@hotmail.com>
2020-02-23 01:00:33 +0100

    Update Class5_2020.java

--- TestGit/src/ch/grabicus/git/second/Class5_2020.java
+++ TestGit/src/ch/grabicus/git/second/Class5_2020.java
@@ -3,6 +3,7 @@ package ch.grabicus.git.second;
 public class Class5_2020 {
 	public static void main(String[] args) {
 		System.out.println("ENTER Class5_2020.main()");
+		System.out.println("Middle of the road");
 		System.out.println("EDIT Class5_2020.main()");
 	}
 }
