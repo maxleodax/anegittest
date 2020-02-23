b2a4bbe7544d34ab5f7b80acc96e31eaef1da2dd
maxleodax <max.leohub@hotmail.com>
2020-02-23 02:47:51 +0100

    Update ANewClass.java

--- TestGit/src/ch/grabicus/git/second/ANewClass.java
+++ TestGit/src/ch/grabicus/git/second/ANewClass.java
@@ -6,6 +6,7 @@ public class ANewClass {
 		System.out.println("ANewClass 1");
 		System.out.println("ANewClass 2");
 		System.out.println("ANewClass 3");
+		System.out.println("ANewClass 4");
 		System.out.println("EXIT main");
 	}
 }
