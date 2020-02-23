e79f789b4b74fd032bf7bb1122e8da311d18ae54
maxleodax <max.leohub@hotmail.com>
2020-02-23 02:42:58 +0100

    Update ANewClass.java
    main updated

--- TestGit/src/ch/grabicus/git/second/ANewClass.java
+++ TestGit/src/ch/grabicus/git/second/ANewClass.java
@@ -2,7 +2,8 @@ package ch.grabicus.git.second;
 
 public class ANewClass {
 	public static void main(String[] args) {
+		System.out.println("ENTER main");
 		System.out.println("ANewClass");
-		System.out.println("EXIT");
+		System.out.println("EXIT main");
 	}
 }
