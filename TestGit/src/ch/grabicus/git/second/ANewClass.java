eb82f90bf368e1eed4f95e210c45f990d4d68ea5
maxleodax <max.leohub@hotmail.com>
2020-02-23 02:40:53 +0100

    ANewClass fixed

--- TestGit/src/ch/grabicus/git/second/ANewClass.java
+++ TestGit/src/ch/grabicus/git/second/ANewClass.java
@@ -1,6 +1,8 @@
 package ch.grabicus.git.second;
 
 public class ANewClass {
-  System.out.println("ANewClass");
-  System.out.println("EXIT");
+	public static void main(String[] args) {
+		System.out.println("ANewClass");
+		System.out.println("EXIT");
+	}
 }
