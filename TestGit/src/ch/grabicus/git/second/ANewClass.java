a02efffac6c629bcbcaeb9f65591a100d28c898e
maxleodax <max.leohub@hotmail.com>
2020-02-23 03:32:50 +0100

    ANewClass 13 local to rebase

--- TestGit/src/ch/grabicus/git/second/ANewClass.java
+++ TestGit/src/ch/grabicus/git/second/ANewClass.java
@@ -18,6 +18,7 @@ public class ANewClass {
 		System.out.println("ANewClass 10 remote");
 		System.out.println("ANewClass 11 local");
 		System.out.println("ANewClass 12 local");
+		System.out.println("ANewClass 13 local to rebase");
 		System.out.println("EXIT main");
 	}
 }
